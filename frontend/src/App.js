d721abb7848c9fe2f0a661202608cddb23d5bdb7
Brandon Weaver <a7xncob@gmail.com>
2020-04-08 17:25:16 -0400

    added navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import { Header } from './components/Header';
+import { NavigationBar } from './components/NavigationBar';
 
 function App() {
   return (
-    <>
-    <Header />
-    </>
+    <Router>
+      <Route path='/' render={() => {
+        return (
+          <>
+          <Header />
+          <NavigationBar />
+          </>
+        );
+      }} />
+    </Router>
   );
 }
 
---
+++ frontend/src/components/NavigationBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import '../styles/NavigationBar.css';
+
+export const NavigationBar = () => {
+	return (
+		<div className="navigation-bar">
+			<NavLink to='/'>Home</NavLink>
+		</div>
+	);
+}
